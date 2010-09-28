4e55f21b85c873673d3483ccb1c85ddcedf38e0d
Julia C. Skvoreshkova <julia@fontlab.com>
2010-09-28 19:24:49 +0400

    main.cpp added

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/***********************************************************************
+ ** Fontlab Ltd.
+ ** Yuri Yarmola
+ ** Copyright (c) 2010
+ **********************************************************************/
+
+#include <iostream>
+
+int main (int argc, char * const argv[]){
+	if (argc < 2)
+    printf("{\"error_code\":\"NO_SOURCE_FILENAME\"}\n");
+  else
+  	printf("{\"error_code\":\"OK\", \"result_name\":\"%s\"}\n", argv[1]);
+    
+  return 0;
+ }
